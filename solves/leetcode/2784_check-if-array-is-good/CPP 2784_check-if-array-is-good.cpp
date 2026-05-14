b9315d832728f78a2348a7771578140449cf9092
MdHamzaHossain <160699277+MdHamzaHossain@users.noreply.github.com>
2026-05-14 21:04:50 +0600

    day 524

--- solves/leetcode/2784_check-if-array-is-good/CPP 2784_check-if-array-is-good.cpp
+++ solves/leetcode/2784_check-if-array-is-good/CPP 2784_check-if-array-is-good.cpp
@@ -17,3 +17,23 @@ public:
         return true;
     }
 };
+//* non-sort
+class Solution
+{
+public:
+    bool isGood(const vector<int> &nums)
+    {
+        const int n = nums.size();
+        bitset<201> bs;
+        for (auto &e : nums)
+        {
+            if (e > n - 1)
+                return false;
+            if (e == n - 1 && bs[e] == 1)
+                bs[0] = 1;
+
+            bs[e] = 1;
+        }
+        return bs.count() == n;
+    }
+};
--- solves/leetcode/2784_check-if-array-is-good/README.md
+++ solves/leetcode/2784_check-if-array-is-good/README.md
@@ -12,6 +12,7 @@ EASY
 - Hash Table
 - Sorting
 
-## __COMMENTS__
+## **COMMENTS**
 
-> sort, check if every index's number is index+1, and see if the final element is the size -1
+> sort, check if every index's number is index+1, and see if the final element is the size -1 2 non sort. bitset method.
+> see if the set bits are the same as n.
